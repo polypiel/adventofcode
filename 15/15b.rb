eb10a77ee53f5227cca0c71f8bacb349de2a3fd9
Angel Calvo <hi@angeluiscalvo.com>
2022-01-09 21:16:44 +0100

    Fix A* algorithm

--- 15/15b.rb
+++ 15/15b.rb
@@ -2,7 +2,6 @@ INIT_POS = 0
 REPS = 5
 
 template = Array.new
-
 $templateHeight = 0
 $templateWidth = 0
 
@@ -22,8 +21,7 @@ cave = Array.new(W * H) do |i|
 	x = (i / W) % $templateWidth
 	y = (i % W) % $templateWidth
 	temp = template[x * $templateWidth + y] + 
-		(( i / W ) / $templateWidth) + 
-			((i % W)/ $templateWidth)
+		((i / W) / $templateWidth) + ((i % W) / $templateWidth)
 	if temp < 10
 		temp
 	else
@@ -31,57 +29,36 @@ cave = Array.new(W * H) do |i|
 	end
 end
 
-# H.times do |i|
-# 	W.times do |j|
-# 		print cave[i * W + j]
-# 	end
-# 	puts
-# end
+Neighbour = Struct.new(:pos, :dist)
 
-
-def getNeighbours(pos)
-	i = pos / W
-	j = pos % W
+def getNeighbours(node)
+	i = node.pos / W
+	j = node.pos % W
 	neighbours = []
-	if (i > 0)
-		neighbours << (i - 1) * W + j
-	end
-	if (j > 0)
-		neighbours << W * i + j - 1
-	end
-	if (i < H - 1)
-		neighbours << (i + 1) * W + j
-	end
-	if (j < W - 1)
-		neighbours << W * i + j + 1
-	end
+	neighbours << Neighbour.new((i - 1) * W + j, node.dist + 1) if (i > 0)
+	neighbours << Neighbour.new(W * i + j - 1, node.dist + 1) if (j > 0)
+	neighbours << Neighbour.new((i + 1) * W + j, node.dist - 1) if (i < H - 1)
+	neighbours << Neighbour.new(W * i + j + 1, node.dist - 1) if (j < W - 1)
 	return neighbours
 end
 
-def getDistance(pos)
-	i = pos / W
-	j = pos % W
-	return (W - 1 - i) + (H - 1 - j)
-end
-
-toVisit = [INIT_POS]
+toVisit = [Neighbour.new(INIT_POS, W - 1 + H - 1)]
 bestPaths = Array.new(W * H) { |i| i == INIT_POS ? 0 : nil }
-best = nil
+best = (W + H) * 9
 
 while(!toVisit.empty?) do
 	current = toVisit.pop()
 	neighbours = getNeighbours(current)
-	neighbours.each do |n|
-		risk = bestPaths[current] + cave[n]
-		if ((bestPaths[n] == nil || bestPaths[n] > risk) && 
-				(best == nil || risk + getDistance(n) < best))
+	neighbours.each do |neigh|
+		n = neigh.pos
+		risk = bestPaths[current.pos] + cave[n]
+		if ((bestPaths[n] == nil || bestPaths[n] > risk) && (risk + neigh.dist) < best)
 			bestPaths[n] = risk
 			if (n != END_POS)
-				toVisit << n
+				toVisit << neigh
+				toVisit.sort! { |a, b| a.dist <=> b.dist}
 			else
-				if(best == nil || risk < best)
-					best = risk
-				end
+				best = risk if (risk < best)
 			end
 		end
 	end
